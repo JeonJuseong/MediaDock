31ac50b48014095cccef22d6ab2883c9a3c0b779
JeonJuseong <mo66jyp@gmail.com>
2026-06-23 19:26:29 +0900

    Initial Commit

---
+++ youtube-downloader/youtube_downloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_downloaderApp.swift
+//  youtube-downloader
+//
+//  Created by 전주성 on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct youtube_downloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
